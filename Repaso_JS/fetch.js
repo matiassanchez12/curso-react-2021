90e906ab466efb2b5f778a5ca37c840202af3200
Matias <matias.sanchez.1897@hotmail.com>
2021-10-04 12:22:39 -0300

    proj-react

--- Repaso_JS/fetch.js
+++ Repaso_JS/fetch.js
@@ -1,5 +1,23 @@
 const url = 'https://jsonplaceholder.typicode.com/users'
 
-fetch(url)
-    .then((response) => response.json())
-    .then(data => console.log(data))
+// fetch(url)
+//     .then((response) => response.json())
+//     .then(data => console.log(data))
+
+const fn = async() => {
+    const response = await fetch(url,{
+        method: 'GET',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Authorization' : 'Bearer asdfasdf'
+        },
+        body: JSON.stringify({
+            name: 'example',
+            website: 'google.com'
+        })
+    })
+    const data = await response.json()
+    console.log(data)
+}
+
+fn()
